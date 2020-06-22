f19133721b3c7b94dc8f1b461018e44dddf3c9a4
hoalamoa <hoacmgch18105@gmail.com>
2020-06-22 11:10:14 +0700

    update

--- product.js
+++ product.js
@@ -34,12 +34,18 @@ router.post('/doInsert',async(req,res)=>{
     let nameValue = req.body.txtName;
     let priceValue = req.body.txtPrice;
     let colorValue = req.body.txtColor;
+    if(colorValue!="Green"||colorValue!="Yellow"||colorValue!="Blue"){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('color should be one of 3 type: Green, Blue or Yellow!');
+    }
+    else{
     let newProduct = {name : nameValue, price : priceValue,color:colorValue};
     await dbo.collection("products").insertOne(newProduct);
     console.log(newProduct);
     // let results = await dbo.collection("products").find({}).toArray();
     // res.render('index',{products:results});
-    res.redirect('/');
+    res.redirect('/');  
+    }
 });
 router.get('/update',async(req,res)=>
 {
